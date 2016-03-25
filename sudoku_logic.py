ddff4366a8a63067787531e2ce8ce4e4a7822941
Vincent Chan <contact@vincentchan.co.uk>
2016-03-25 23:23:39 +0000

    added some validation

--- sudoku_logic.py
+++ sudoku_logic.py
@@ -1,4 +1,24 @@
 """ This is the logic component for the sudoku solver """
 
-def calculate(values):
-    print values
+def calculate(puzzle):
+    errors = validate(puzzle)
+
+    if len(errors) is not 0:
+        print "blah"
+
+def validate(puzzle):
+    errors = []
+
+    for gridNumber, grid in enumerate(puzzle):
+        gridErrors = {}
+        for gridPosition, value in enumerate(grid):
+            if value.toInt()[1] is False:
+                gridErrors[gridPosition] = ('value must be numeric')
+
+        errors.append(gridErrors)
+
+    for gridErrors in errors:
+        if len(gridErrors) is not 0:
+            return errors
+
+    return []
