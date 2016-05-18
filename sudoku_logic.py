f9837a1e3c0ca6c0b96d1ebba096332789e6283f
Vincent Chan <contact@vincentchan.co.uk>
2016-05-18 18:22:21 +0100

    some extra stuff

--- sudoku_logic.py
+++ sudoku_logic.py
@@ -1,4 +1,20 @@
 """ This is the logic component for the sudoku solver """
 
 def calculate(values):
-    print values
+    if validate(values) != True:
+        """ Exit! """
+        return
+
+    
+
+
+def validate(values):
+    validated = True
+
+    for box in values:
+        for valueObj in box:
+            print valueObj
+            print "\n"
+        print "\n"
+
+    return validated
