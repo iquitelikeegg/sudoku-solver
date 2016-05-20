cd89ab203d7a3120252b721050062f36e06af77d
Vincent Chan <contact@vincentchan.co.uk>
2016-05-20 01:20:09 +0100

    possiblity gridbegun

--- sudoku_logic.py
+++ sudoku_logic.py
@@ -1,5 +1,7 @@
 """ This is the logic component for the sudoku solver """
 
+import math
+
 def calculate(values):
     if validate(values) != True:
         """ Exit! """
@@ -17,14 +19,35 @@ def calculationPass(gridArray):
 
     for i in range (1, 10):
         # Go throgh each number in turn
+        print "checking for %d" % i
 
-        continue
+        possibilityGrid = getPossibilityGrid()
+
+        for rowNumber, row in enumerate(rows):
+            if i in row:
+                # First mark the row
+                gridRow = int(math.floor(rowNumber / 3))
+                subGridRow = rowNumber if rowNumber < 3 else (rowNumber % 3)
+
+                print gridRow
+                print subGridRow
+
+                for j, possibilityGridBox in enumerate(possibilityGrid):
+                    if j in [3 * gridRow, 3 * gridRow + 1, 3 * gridRow + 2]:
+                        possibilityGridBox[3 * subGridRow] = 0
+                        possibilityGridBox[3 * subGridRow + 1] = 0
+                        possibilityGridBox[3 * subGridRow + 2] = 0
+
+
+        # Then mark the grid
+
+        print possibilityGrid
 
 def getRows(gridArray):
     rows = []
 
-    for i in range(0, 2):
-        for j in range(0, 2):
+    for i in range(0, 3):
+        for j in range(0, 3):
             row = []
             row.append(gridArray[3 * i][3 * j])
             row.append(gridArray[3 * i][3 * j + 1])
@@ -59,6 +82,19 @@ def getColumns(gridArray):
             columns.append(column)
 
     return columns
+
+def getPossibilityGrid():
+    return [
+        [1,1,1,1,1,1,1,1,1],
+        [1,1,1,1,1,1,1,1,1],
+        [1,1,1,1,1,1,1,1,1],
+        [1,1,1,1,1,1,1,1,1],
+        [1,1,1,1,1,1,1,1,1],
+        [1,1,1,1,1,1,1,1,1],
+        [1,1,1,1,1,1,1,1,1],
+        [1,1,1,1,1,1,1,1,1],
+        [1,1,1,1,1,1,1,1,1]
+    ]
 
 def validate(values):
     validated = True
